6b08dd3b6fd5e15eab56a6f5a6164c3dc1f4836d
suriyon <suriyon.s@ubru.ac.th>
2025-12-09 09:48:58 +0700

    first commit

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return f'<h1>Hello World!</h1>'
+
+@app.route('/name')
+def name():
+  return f'<h1>Suriyon Saramul</h1>'
+
+# if __name__ == '__main__':
+#   app.run(debug=True)
